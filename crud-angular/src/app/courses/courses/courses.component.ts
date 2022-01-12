27ca824047f1e9b1b61de3ca0ce3cb87ae5b51e4
joabehenrique <henriquecidoz@hotmail.com>
2022-01-11 22:11:27 -0300

    feat: adicionado template para exibir os cursos e sua categoria

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [
+    { _id: '1', name: 'Hydrogen', category: 'H' },
+    { _id: '2', name: 'Helium', category: 'He' },
+    { _id: '3', name: 'Lithium', category: 'Li' }
+  ];
+  displayedColumns = ['name', 'category'];
 
   constructor() { }
 
