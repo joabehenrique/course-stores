45617b0bdd0d563369b683833bf5799adfe142e6
joabehenrique <henriquecidoz@hotmail.com>
2022-03-02 23:51:28 -0300

    chore: adicionado commandLineRunner para popular o banco de teste

--- crud-spring/src/main/java/com/loiane/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/loiane/crudspring/CrudSpringApplication.java
@@ -1,7 +1,11 @@
 package com.loiane.crudspring;
 
+import com.loiane.crudspring.entity.Course;
+import com.loiane.crudspring.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +14,14 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository repository){
+		return args -> {
+			repository.deleteAll();
+			Course course = new Course();
+			course.setName("JavaScript");
+			course.setCategory("Frontend");
+			repository.save(course);
+		};
+	}
 }
